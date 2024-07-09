2ef38e159e3c693a8a2593b38e8b27b3058c89a9
Garima Sharma <sharmagarima814@gmail.com>
2024-07-09 22:01:13 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,59 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuid4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+const loadTasks = (): Task[] => {
+  const allTasks = localStorage.getItem('TASKS');
+  if (!allTasks) return [];
+  return JSON.parse(allTasks);
+};
+
+const addListItem = (task: Task): boolean => {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+  return true;
+};
+
+const tasksList: Task[] = loadTasks();
+tasksList.forEach(addListItem);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const task: Task = {
+    id: uuid4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasksList.push(task);
+  saveTasks();
+
+  addListItem(task);
+  input.value = '';
+});
+
+const saveTasks = () => {
+  localStorage.setItem('TASKS', JSON.stringify(tasksList));
+};
